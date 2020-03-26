cf017c4d4ca78bd071c82548e88d30224e236e24
Michael Kastner <kastner@galt.de>
2020-03-26 09:13:15 +0100

    added gif test

--- test/index.test.js
+++ test/index.test.js
@@ -15,14 +15,21 @@ async function main() {
       const pngResult = deconstructBase64(base64PNG);
       t.equals('image/png', pngResult.mimeType);
       t.equals('png', pngResult.extension);
-      t.ok(pngResult.data, 'should extract data');
+      t.ok(pngResult.data, 'should extract png data');
 
       const base64JPG = await fs.readFile(
         path.join(__dirname, 'assets', 'example-mini-jpg.base64'), 'utf8');
       const jpgResult = deconstructBase64(base64JPG);
       t.equals('image/jpeg', jpgResult.mimeType);
       t.equals('jpg', jpgResult.extension);
-      t.ok(jpgResult.data, 'should extract data');
+      t.ok(jpgResult.data, 'should extract jpg data');
+      
+      const base64GIF = await fs.readFile(
+        path.join(__dirname, 'assets', 'example-mini-gif.base64'), 'utf8');
+      const gifResult = deconstructBase64(base64GIF);
+      t.equals('image/gif', gifResult.mimeType);
+      t.equals('gif', gifResult.extension);
+      t.ok(gifResult.data, 'should extract gif data');
 
     } catch (err) {
       log.error(err);
